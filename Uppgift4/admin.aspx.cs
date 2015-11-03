14c394ce58014c3c3f184ae44ecd46c5b9f79ea9
eklund90 <johaneklund90@gmail.com>
2015-11-03 16:38:30 +0100

    kjhl

--- Uppgift4/admin.aspx.cs
+++ Uppgift4/admin.aspx.cs
@@ -24,28 +24,27 @@ namespace Uppgift4
         private void laddatestare()
         {
                 
-                sql = "select * from webbutveckling.anvandare";
+                sql = "select * from webbutveckling.test";
                 NpgsqlConnection conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["uppgift4"].ConnectionString);
                 NpgsqlCommand command;
                 conn.Open();
                 command = new NpgsqlCommand(sql, conn);            
                 NpgsqlDataReader dr = command.ExecuteReader();
 
-                string hej = null;
+
                 while (dr.Read())
                 {
-                    hej += dr["anvandarnamn"].ToString();
-                    //Anvandare adminanv = new Anvandare();
-                    //Anvandare.anvandarnamn = dr["anvandarnamn"].ToString();
-                    //Anvandare.losenord = dr["losenord"].ToString();
-                    //Anvandare.licensiering = dr["licensierad"].ToString();
-                    ////Anvandare.kunskapstest = dr["kunskapstest"].ToString();
-                    ////Anvandare.datumkunskapstest = dr["datumkunskapstest"];
-                    //Anvandare.roll = dr["roll"].ToString();
-                    //listadminanv.Add(adminanv);
+                    Anvandare adminanv = new Anvandare();
+                    Anvandare.anvandarnamn = dr["anvandarnamn"].ToString();
+                    Anvandare.losenord = dr["losenord"].ToString();
+                    Anvandare.licensiering = dr["licensierad"].ToString();
+                    //Anvandare.kunskapstest = dr["kunskapstest"].ToString();
+                    //Anvandare.datumkunskapstest = dr["datumkunskapstest"];
+                    Anvandare.roll = dr["roll"].ToString();
+                    listadminanv.Add(adminanv);
                 }
 
-                admingrid.DataSource = hej;
+                admingrid.DataSource = listadminanv;
                 admingrid.DataBind();
                 
                 
